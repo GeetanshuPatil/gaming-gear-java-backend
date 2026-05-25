e2c623062d9869b934783adf38b2f7815cdb3583
Geetanshup08 <geetanshup08@gmail.com>
2026-05-25 14:26:43 +0530

    fix render time 02

--- src/main/java/com/geetanshu/gaminggearshop/controller/TestController.java
+++ src/main/java/com/geetanshu/gaminggearshop/controller/TestController.java
@@ -1,17 +1,14 @@
 package com.geetanshu.gaminggearshop.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Gaming Gear Shop Backend Running";
-    }
     @GetMapping("/health")
-    public String health() {
-        return "OK";
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Backend is awake");
     }
 }
